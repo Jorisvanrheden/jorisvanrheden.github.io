18175d210064bd5cc1540d6009d7cb41d8335f91
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-07-23 23:48:59 +0200

    add interstellar entry

--- src/Logic/ProjectInformation/YoutubeProjects.tsx
+++ src/Logic/ProjectInformation/YoutubeProjects.tsx
@@ -1,53 +1,58 @@
-export default interface YoutubeProjectData
-{
-    url:string;
-    description:string,
-    image:string;
-    location:string;
+export default interface YoutubeProjectData {
+    url: string;
+    description: string,
+    image: string;
+    location: string;
 }
 
 export const YoutubeProjectEntries: Array<YoutubeProjectData> =
-[
-    {
-        url: "https://www.youtube.com/embed/Kmu96X8t9F0",
-        description: "Death Note - Low of Solipsism",
-        image: "music.png",
-        location: "sheet_music/Death Note - Low of Solipsism.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/L0et5Yf25h8",
-        description: "One Piece - Becoming the Pirate King",
-        image: "image.png",
-        location: "sheet_music/One Piece - Becoming the Pirate King.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/WNMRrK5s5zc",
-        description: "One Piece - Difficult",
-        image: "image.png",
-        location: "sheet_music/One Piece - Difficult.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/hrvGv3K-t68",
-        description: "One Piece - The Very Very Very Strongest",
-        image: "image.png",
-        location: "sheet_music/One Piece - The Very Very Very Strongest.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/9ZwATJgLNKc",
-        description: "One Piece - The Worlds Number One",
-        image: "image.png",
-        location: "sheet_music/One Piece - The Worlds Number One.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/49xRC3QFnvg",
-        description: "One Piece - To the Grand Line",
-        image: "image.png",
-        location: "sheet_music/One Piece - To the Grand Line.pdf"
-    },
-    {
-        url: "https://www.youtube.com/embed/UHXxULlzd_4",
-        description: "One Piece - Wake Up",
-        image: "image.png",
-        location: "sheet_music/One Piece - Wake Up.pdf"
-    },
-]
+    [
+        {
+            url: "",
+            description: "Interstellar - Main Theme",
+            image: "music.png",
+            location: "sheet_music/Interstellar - Main Theme.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/Kmu96X8t9F0",
+            description: "Death Note - Low of Solipsism",
+            image: "music.png",
+            location: "sheet_music/Death Note - Low of Solipsism.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/L0et5Yf25h8",
+            description: "One Piece - Becoming the Pirate King",
+            image: "image.png",
+            location: "sheet_music/One Piece - Becoming the Pirate King.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/WNMRrK5s5zc",
+            description: "One Piece - Difficult",
+            image: "image.png",
+            location: "sheet_music/One Piece - Difficult.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/hrvGv3K-t68",
+            description: "One Piece - The Very Very Very Strongest",
+            image: "image.png",
+            location: "sheet_music/One Piece - The Very Very Very Strongest.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/9ZwATJgLNKc",
+            description: "One Piece - The Worlds Number One",
+            image: "image.png",
+            location: "sheet_music/One Piece - The Worlds Number One.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/49xRC3QFnvg",
+            description: "One Piece - To the Grand Line",
+            image: "image.png",
+            location: "sheet_music/One Piece - To the Grand Line.pdf"
+        },
+        {
+            url: "https://www.youtube.com/embed/UHXxULlzd_4",
+            description: "One Piece - Wake Up",
+            image: "image.png",
+            location: "sheet_music/One Piece - Wake Up.pdf"
+        },
+    ]
