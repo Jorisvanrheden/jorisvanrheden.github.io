453ed4c47ae6b0d0642b15cb0bb90bcf2a4eec88
Joris van Rheden <jorisvanrheden@hotmail.com>
2022-10-27 21:24:06 +0200

    cleanup unused files

--- src/Components/ProjectContainer/ProjectContainer.tsx
+++ src/Components/ProjectContainer/ProjectContainer.tsx
@@ -1,7 +1,6 @@
 import "./ProjectContainer.css"
 
 import Dropdown from '../Dropdown/Dropdown'
-import SheetDisplayer from "../SheetDisplayer/SheetDisplayer"
 import ProjectContainerItem from "./ProjectContainerItem/ProjectContainerItem"
 
 import {YoutubeProjectEntries} from "./../../Logic/ProjectInformation/YoutubeProjects"
@@ -27,10 +26,6 @@ export default function ProjectContainer()
             <div id="video">
                 <ProjectContainerItem entry={YoutubeProjectEntries[index]}/>
             </div>
-            {/* <div id="sheet">
-                <SheetDisplayer image={YoutubeProjectEntries[index].image}
-                                location={YoutubeProjectEntries[index].location}/>
-            </div> */}
         </div>     
     )
 }
